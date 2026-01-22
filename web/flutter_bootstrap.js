3ef204bdd63e46682362d6d270efafd5a26a25c3
unknown <aslanbekbakberdi@gmail.com>
2026-01-22 05:19:41 +0500

    F

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3353896875"
+    serviceWorkerVersion: "3701591726"
   }
 });
